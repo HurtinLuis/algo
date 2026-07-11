bda256aa9bac4cca813a05671770e0aca54427cf
HurtinLuis <yan_shuojian@163.com>
2026-07-11 21:42:16 +0800

    [l, r] -> [l, r)

--- data/Fenwick.cpp
+++ data/Fenwick.cpp
@@ -3,21 +3,47 @@ struct Fenwick {
     int n;
     std::vector<T> a;
 
-    Fenwick(int n = 0) : n(n), a(n + 1, T{}) {}
+    Fenwick(int n = 0) {
+        init(n);
+    }
 
-    void add(int x, T v) {
-        for (int i = x; i <= n; i += i & -i) {
-            a[i] += v;
+    void init(int n_) {
+        n = n_;
+        a.assign(n + 1, T{});
+    }
+
+    void add(int x, const T &v) {
+        assert(0 <= x && x < n);
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i] = a[i] + v;
         }
     }
 
     T sum(int x) {
+        assert(0 <= x && x <= n);
         T ans{};
         for (int i = x; i > 0; i -= i & -i) {
-            ans += a[i];
+            ans = ans + a[i];
         }
         return ans;
     }
 
-    T query(int l, int r) { return sum(r) - sum(l - 1); }
+    T query(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        return sum(r) - sum(l);
+    }
+
+    // returns the longest prefix length x such that sum(x) <= k.
+    int maxPrefix(const T &k) {
+        T cur{};
+        int x = 0;
+        for (int i = 1 << std::__lg(n); i; i >>= 1) {
+            if (x + i <= n && cur + a[x + i] <= k) {
+                cur = cur + a[x + i];
+                x += i;
+            }
+        }
+        assert(0 <= x && x <= n);
+        return x;
+    }
 };
--- data/SegmentTree.cpp
+++ data/SegmentTree.cpp
@@ -75,7 +75,7 @@ struct SegmentTree {
     }
 
     Info query(int l, int r) {
-        return query(1, 0, n, l, r + 1);
+        return query(1, 0, n, l, r);
     }
 
     void rangeApply(int p, int l, int r, int x, int y, const Tag &v) {
@@ -94,7 +94,57 @@ struct SegmentTree {
     }
 
     void rangeApply(int l, int r, const Tag &v) {
-        return rangeApply(1, 0, n, l, r + 1, v);
+        return rangeApply(1, 0, n, l, r, v);
+    }
+
+    template <class F>
+    int findFirst(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        push(p);
+        int m = (l + r) / 2;
+        int res = findFirst(2 * p, l, m, x, y, pred);
+        if (res == -1) {
+            res = findFirst(2 * p + 1, m, r, x, y, pred);
+        }
+        return res;
+    }
+
+    template <class F>
+    int findFirst(int l, int r, F &&pred) {
+        return findFirst(1, 0, n, l, r, pred);
+    }
+
+    template <class F>
+    int findLast(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        push(p);
+        int m = (l + r) / 2;
+        int res = findLast(2 * p + 1, m, r, x, y, pred);
+        if (res == -1) {
+            res = findLast(2 * p, l, m, x, y, pred);
+        }
+        return res;
+    }
+
+    template <class F>
+    int findLast(int l, int r, F &&pred) {
+        return findLast(1, 0, n, l, r, pred);
     }
 };
 
